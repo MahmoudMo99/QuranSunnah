ddc251c49abc54b14c9c9609594ff10ebfe8f3c7
Mahmoud Mo <mmahmoud092002@gmail.com>
2024-10-14 11:09:50 +0300

    create and set global spinner ( loader )

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { QuranModule } from './modules/quran/quran.module';
 import { AzkarAndDuaModule } from './modules/azkar-and-dua/azkar-and-dua.module';
 import { BooksAndLecturesModule } from './modules/books-and-lectures/books-and-lectures.module';
 import { HadithModule } from './modules/hadith/hadith.module';
 import { QuestionsAndInfoModule } from './modules/questions-and-info/questions-and-info.module';
+import { MainInterceptor } from './interceptor/main.interceptor';
+import { LoaderComponent } from './components/loader/loader.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, LoaderComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -21,7 +23,13 @@ import { QuestionsAndInfoModule } from './modules/questions-and-info/questions-a
     HadithModule,
     QuestionsAndInfoModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: MainInterceptor,
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
